fdc33debb0a36636b87b5ebb717000103ee3933d
Vladyslav Khrystych <vkhrystych@gmail.com>
2021-07-06 14:23:38 +0300

    Added solution for module 5 task 1

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,17 +1,38 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  const isNestedPath = pathname.includes('/');
+
+  if (isNestedPath) {
+    res.statusCode = 400;
+    res.end();
+  }
+
+  const fileStream = fs.createReadStream(filepath);
+
+  fileStream.on('data', (chunk) => {
+    res.write(chunk);
+  });
+
+  fileStream.on('error', () => {
+    res.statusCode = 404;
+    res.end();
+  });
+
+  fileStream.on('end', () => {
+    res.end();
+  });
 
   switch (req.method) {
     case 'GET':
-
       break;
 
     default:
