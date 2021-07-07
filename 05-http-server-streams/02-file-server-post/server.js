72c0778f7921a8f884e39b2faf3639f01a024858
vkhrystych-perion <vladyslavk@Perion.com>
2021-07-07 10:54:46 +0300

    Added solution for module 5 task 2

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,17 +1,65 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  const isNestedPath = pathname.includes('/') || pathname.includes('\\');
+  if (isNestedPath) {
+    res.statusCode = 400;
+    res.end('Nested pathes are not supported');
+
+    return;
+  }
+
+  const isFileAlreadyExists = fs.existsSync(filepath);
+
+  if (isFileAlreadyExists) {
+    res.statusCode = 409;
+    res.end('File with this name already exist');
+
+    return;
+  }
+
+  const oneMegabyteInBytes = 1048576;
+  const writeFileStream = fs.createWriteStream(filepath);
+  const streamWithLimit = new LimitSizeStream({
+    limit: oneMegabyteInBytes,
+  });
+
+  const streamWithLimitErrorCb = () => {
+    fs.unlinkSync(filepath);
+
+    res.statusCode = 413;
+    res.end('Maximum file size is 1mb');
+  };
+
+  const onRequestAbortedCb = () => {
+    writeFileStream.destroy();
+    fs.unlinkSync(filepath);
+  };
+
+  const onStreamFinishCb = () => {
+    res.statusCode = 201;
+    res.end('Success! File was created');
+  }
+
+  req.on('aborted', onRequestAbortedCb);
+
+  req
+    .pipe(streamWithLimit)
+    .on('error', streamWithLimitErrorCb)
+    .pipe(writeFileStream)
+    .on('finish', onStreamFinishCb);
 
   switch (req.method) {
     case 'POST':
-
       break;
 
     default:
