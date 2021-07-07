63181408c993130ad71d793fe15c0995dec00b21
vkhrystych-perion <vladyslavk@Perion.com>
2021-07-07 11:32:54 +0300

    Added solution for module 5 task 3

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,13 +1,34 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  const isFileExists = fs.existsSync(filepath);
+  const isNestedPath = pathname.includes('/') || pathname.includes('\\');
+
+  if (isNestedPath) {
+    res.statusCode = 400;
+    res.end('Nested pathes are not supported');
+
+    return;
+  }
+
+  if (!isFileExists) {
+    res.statusCode = 404;
+    res.end('Cannot find file with this name');
+
+    return;
+  }
+
+  fs.unlinkSync(filepath);
+  res.statusCode = 200;
+  res.end('Success! File was deleted');
 
   switch (req.method) {
     case 'DELETE':
