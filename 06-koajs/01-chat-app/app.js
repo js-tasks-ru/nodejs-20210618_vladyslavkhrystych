bc64aed97a1e6667eb8920d0b82ebb690ed0eec8
Vladyslav Khrystych <vladyslavkhrystych@gmail.com>
2021-07-15 17:55:38 +0300

    Added solution for module 6 task 1

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,27 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+let subscribers = [];
+
+router.get('/subscribe', async (ctx) => {
+  const delayOnSubscribe = new Promise((resolve) => {
+    subscribers.push(resolve);
+  });
+
+  const message = await delayOnSubscribe;
+  ctx.response.body = message;
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post('/publish', async (ctx) => {
+  if (!ctx.request.body.message) {
+    return;
+  }
+
+  subscribers.forEach((subscriber) => {
+    subscriber(ctx.request.body.message);
+  });
+  ctx.status = 200;
+  subscribers = [];
 });
 
 app.use(router.routes());
