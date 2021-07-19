5bbee487ff520c4e1f433c1d9fe5ea2770f03ac3
Vladyslav Khrystych <vladyslavkhrystych@gmail.com>
2021-07-19 16:15:33 +0300

    Added solution for module 7 task 2

--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,57 @@
-module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+const ObjectId = require('mongoose').Types.ObjectId;
+const Product = require('../models/Product');
+const formatProductForClient = require('../libs/formatProductForClient');
+
+module.exports.productsBySubcategory = async function productsBySubcategory(
+  ctx,
+  next
+) {
+  const subcategory = ctx.query.subcategory;
+  if (subcategory) {
+    const products = await Product.find({
+      subcategory,
+    });
+
+    const productsFormatted = products.map((product) =>
+      formatProductForClient(product)
+    );
+
+    ctx.body = {
+      products: productsFormatted,
+    };
+  } else {
+    await next();
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find();
+  const productsFormatted = products.map((product) =>
+    formatProductForClient(product)
+  );
+
+  ctx.body = {
+    products: productsFormatted,
+  };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  const productId = ctx.params.id;
 
+  if (ObjectId.isValid(productId)) {
+    const product = await Product.findOne({ _id: productId });
+
+    if (!product) {
+      ctx.status = 404;
+      return;
+    }
+
+    const productFormatted = formatProductForClient(product);
+
+    ctx.body = {
+      product: productFormatted,
+    };
+  } else {
+    ctx.status = 400;
+  }
+};
